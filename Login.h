1599acf0b6227693c29ffe9eec7fb3937e862d8e
Kawaiine18 <167618690+Kawaiine18@users.noreply.github.com>
2024-09-14 12:41:51 +0300

    Update_Login.h

--- Login.h
+++ Login.h
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char* allouer(int d);
-char* allouer_2(int d);
-void getdata(char* nom,char* password);
+//dimension de tous les tableaux
+#define d 255	
+//Entrez nom et mot de passe
+void getdata_name(char** name);
+void getdata_password(char** password);
+//ouverture du fichier contenant les noms et mot de passe
 FILE* ouverture(FILE* file);
-void traitement(FILE* file,char* nom,char* password,int d);
-void comparer(FILE* file,char* nom,char* password,int d);
+//Ecrire dans le fichier pour s'inscrire
+void inscription(char* name,char* password,FILE* file);
+//Pour se connecter, s'inscrire si n'a pas de compte
+void connecter(char* name,char* password,FILE* file);
+//liberer tous les tableaux
+void Free_All_Array(char* name,char* password);
+//pour s'inscrire ou se connecter
+void menu(char* name,char* password,FILE* file);
